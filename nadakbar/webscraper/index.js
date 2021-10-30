568b828a078b956d258922ed9647be94cbe621c1
Nadeem <nadeemakbar786@gmail.com>
2021-10-30 11:47:54 +0100

    ✨Get Response ✨ Set new URL ➕ Axios

--- nadakbar/webscraper/index.js
+++ nadakbar/webscraper/index.js
@@ -1,8 +1,18 @@
 const PORT = 8000
+const { default: axios } = require("axios");
 const axis = require("axios")
-const cheerio = require("cheerio")
+const cheerio = require("cheerio");
+const { response } = require("express");
 const express = require("express")
 
 const app = express();
+
+const URL = "https://www.coingecko.com/en"
+
+axios(URL)
+    .then(response => {
+        const html = response.data
+        console.log(html)
+    })
 
 app.listen(PORT, ()=> console.log(`Server running on PORT ${PORT}`))
